27ea661a4fc3d2fe5da8fc47d9eca958d1b7f18b
Pc <tusinglawrencejohn@gmail.com>
2023-12-31 00:28:56 +0800

    may adding users na

--- dashboard.php
+++ dashboard.php
@@ -12,7 +12,10 @@ include('includes/header.php');
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
-            Dashboard
+        <h1 class="display-1">Dashboard</h1>
+            <a href="users.php">Users Management </a> | 
+            <a href="posts.php">Posts Management </a>
+            
         </div>
 
     </div>
