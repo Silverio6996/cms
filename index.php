7062112d3b78ada22ce193c3156a8ae799c6e582
Pc <tusinglawrencejohn@gmail.com>
2023-12-30 17:15:25 +0800

    editing the cms

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "Hello World" ?>
+<?php 
+
+include('includes/config.php');
+include('includes/database.php');
+include('includes/functions.php');
+
+include('includes/header.php');
+?>
+
+This is the content of the index.php file
+
+<?php
+include('includes/footer.php');
+?>
